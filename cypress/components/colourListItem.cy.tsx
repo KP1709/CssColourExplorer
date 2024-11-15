ca4ee9f3dea4f774803c068ce20bfd603af2d5a7
Kareena Patel <kareenapat17@gmail.com>
2024-11-15 14:03:39 +0000

    Add further tests to ColourListItem

--- cypress/components/colourListItem.cy.tsx
+++ cypress/components/colourListItem.cy.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ColourListItem from "../../src/components/colourListItem"
 import "../../src/styles/colourListItem.css"
+import "../../src/styles/colourList.css"
 import { mount } from 'cypress/react18'
 
 describe('<ColourListItem />', () => {
-  it('renders Tan list item', () => {
-    mount(<ul><li><ColourListItem colourName={'Dark'} hexColour={'483D8B'} /></li></ul>)
+  it('renders colourListItems', () => {
+    mount(<ul>
+      <li><ColourListItem colourName={'Dark Slate Blue'} hexColour={'483D8B'} /></li>
+      <li><ColourListItem colourName={'Dark Cyan'} hexColour={'008B8B'} /></li>
+      <li><ColourListItem colourName={'Medium Violet Red'} hexColour={'C71585'} /></li>
+      <li><ColourListItem colourName={'Rebecca Purple'} hexColour={'663399'} /></li>
+      <li><ColourListItem colourName={'Rosy Brown'} hexColour={'BC8F8F'} /></li>
+    </ul>
+    )
   })
 })
